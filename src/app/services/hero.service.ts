14dad2b367b9ec2d84d78b16351500ef41caf99c
epow <evanpowellmusic@gmail.com>
2018-10-31 13:49:22 -0500

    update messages

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -3,15 +3,17 @@ import { Observable, of } from 'rxjs';
 
 import { Hero } from '../models/hero';
 import { Heroes } from '../mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
     return of(Heroes);
   }
 }
