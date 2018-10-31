3df585608e80803d40a44c5d3978172bb459b4d5
epow <evanpowellmusic@gmail.com>
2018-10-31 11:09:51 -0500

    bring mock heroes data into hero service

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 
+import { Hero } from '../models/hero';
+import { Heroes } from '../mock-heroes';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
   constructor() { }
+
+  getHeroes(): Hero[] {
+    return Heroes;
+  }
 }
